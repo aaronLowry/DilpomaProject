1ff0c36a5ece7b453460a89630406c96bb1622f5
a.lowry <aaron.lowry@live.com.au>
2014-05-10 20:02:17 +1000

    Added:
    Add card method
    Empty hand method

--- CardCasino/src/au/com/Blackjack/GameRules/Blackjack.java
+++ CardCasino/src/au/com/Blackjack/GameRules/Blackjack.java
@@ -1,15 +1,12 @@
 package au.com.Blackjack.GameRules;
 
-import java.lang.reflect.Method;
-
-import au.com.Blackjack.Deck.Deck;
-
 //Rules Class
 public abstract class Blackjack extends Rules {
 
 	// Properties of the player
-	static int betPlaced; // The bet that the player has made prior to dealing of the cards.
-	int playerWinnings;	
+	double betPlaced; // The bet that the player has made prior to dealing of the cards.
+	double playerWinnings;
+	Object[] playerHand = new Object[0];
 	
 	// Properties of the Blackjack class
 	public final static int HIGHEST_SCORE = 21;
@@ -19,31 +16,11 @@ public abstract class Blackjack extends Rules {
 	// Check up on these rules about the dealer sitting or hitting on 17, ie.
 	// Soft sitting and  hard hitting
 	public final static int DEALER_SOFT_HARD_17 = 17;
-	public final static boolean IS_SOFT_17 = false;
-	public final static boolean IS_HARD_17 = false;
+	//public final static boolean IS_SOFT_17 = false;
+	//public final static boolean IS_HARD_17 = false;
 	
-	
-	
-	public void playerCards(int cardCount) {
-		if(cardCount > HIGHEST_SCORE) {
-			// Call playerBust method
-		} else {
-			if(cardCount < HIGHEST_SCORE) {
-				// Player must choose which method to take. ie. Stand, Hit, etc.
-			}
-			else {
-				playerBlackjack();
-			}
-		}
-	}
-	
-	
-	
-	private void playerBlackjack() {
-		playerWinnings = BLACKJACK_ODDS;
-	}
-	
-	private static int getBlackjackOdds(/*Place decksize here*/) {
+	// Get/Set winning odds
+	public int getBlackjackOdds(int deckSize) {
 		if(deckSize < 3) {
 			return 1;
 		}else {
@@ -56,6 +33,33 @@ public abstract class Blackjack extends Rules {
 		}
 	}
 	
-	
-	
+	// Players hand
+	public void addCardToHand(Object card, int CardCount) {
+		playerHand[CardCount] = card;
+	}
+	public void emptyHand(boolean endRound) {
+		if(endRound) {
+			for(int i = 0; i < playerHand.length; i++) {
+				playerHand[i] = null;
+			}
+		}
+	}
+	public void playerCards(int cardCount) {
+		if(cardCount > HIGHEST_SCORE) {
+			playerBust();
+		} else {
+			if(cardCount < HIGHEST_SCORE) {
+				// Player must choose which method to take. ie. Stand, Hit, etc.
+			}
+			else {
+				playerBlackjack();
+			}
+		}
+	}
+	public void playerBust() {
+		playerWinnings = 0;
+	}
+	public void playerBlackjack() {
+		playerWinnings *= blackjackOdds;
+	}
 }
